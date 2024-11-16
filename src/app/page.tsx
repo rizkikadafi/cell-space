347604317111ad6f7a6f2023222cd8ac47e0336f
rizkikadafi <rizkikadafi11@gmail.com>
2024-11-16 15:59:04 +0700

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-"use client"
-
-import { useState } from "react";
 import Navigation from "@/app/ui/navigation"
 import HeroSection from "@/app/ui/hero-section"
-import Loading from "@/app/loading"
 
 export default function Home() {
-  // const [isLoading, setIsLoading] = useState(true);
-
-  // const handleLoad = () => {
-  //   setIsLoading(false);
-  // };
   return (
     <>
       <Navigation />
