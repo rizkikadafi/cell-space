274f041d34fa45aefa52bb18f39c9358a25ac12b
rizkikadafi <rizkikadafi11@gmail.com>
2024-12-01 17:47:19 +0700

    fix route api type

--- src/app/api/course/[id]/route.ts
+++ src/app/api/course/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
